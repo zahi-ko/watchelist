1ad92f7823d25064298b5b640aba3b2d6a82f9d1
Zahi <zahi.robs@gmail.com>
2024-08-27 21:07:04 +0800

    feat: Update index page, error handling, and database support
    This commit updates the index page by removing the user parameter from the render_template function, adds a new 404.html template for handling page not found errors, and establishes a one-many relationship between the user and movies. It also introduces SQLite database support to the Flask app and adds a new Movie model. These changes improve the overall functionality, user experience, and database capabilities of the application.

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 import os
-from re import U
 import sys
 import click
 
-from flask import Flask, render_template
+from flask import Flask, flash, redirect, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 ISWIN = sys.platform.startswith('win')
@@ -13,6 +12,7 @@ else:
     prefix = 'sqlite:////'
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'dev'
 app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -37,10 +37,13 @@ def forge():
     ]
 
     user = User(name=user)
+    db.session.add(user)
+    db.session.commit()
+
     for m in movies:
-        user.movies.append(Movie(title=m['title'], year=m['year'], user_id=user.id))
+        movie = Movie(title=m['title'], year=m['year'], user_id=user.id)
+        db.session.add(movie)
     else:
-        db.session.add(user)
         db.session.commit()
     click.echo('Done.')
 
@@ -63,18 +66,65 @@ class Movie(db.Model):
     year = db.Column(db.String(4))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        title = request.form.get('title')
+        year = request.form.get('year')
+
+        if any((not title, not year, len(year) > 4, len(title) > 60)):
+            flash('Invalid input.')
+            return redirect(url_for('index'))
+        
+        user = User.query.first()
+        movie = Movie(title=title, year=year, user_id=user.id)
+
+        db.session.add(movie)
+        db.session.commit()
+        
+        flash('Item created.')
+        return redirect(url_for('index'))
+    
     user = User.query.first()
     movies = Movie.query.filter_by(user_id=user.id).all()
     return render_template('index.html', movies=movies)
 
 @app.errorhandler(404)
 def page_not_found(e):
-    user = User.query.first()
     return render_template('404.html'), 404
 
 @app.context_processor
 def inject_user():
     user = User.query.first()
     return dict(user=user)
+
+@app.route('/movie/edit/<int:movie_id>', methods=['GET', 'POST'])
+def edit(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        year = request.form['year']
+
+        if any((not title, not year, len(title) > 60, len(year) > 4)):
+            flash('Invalid input.')
+            return redirect(url_for('edit', movie_id=movie_id))
+
+        movie.title = title
+        movie.year = year
+        db.session.commit()
+        flash('Item updated.')
+        return redirect(url_for('index'))
+
+    return render_template('edit.html', movie=movie)
+
+@app.route('/movie/delete/<int:movie_id>', methods=['POST'])
+def delete(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+    db.session.delete(movie)
+    db.session.commit()
+    flash('Item deleted.')
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
